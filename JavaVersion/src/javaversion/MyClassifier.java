86d85e0578eadb5a80b4f442be5438ccacaecdf1
AmiroooTheWalnut <amir.esmaieeli@gmail.com>
2019-10-09 14:14:34 -0700

    Javav1

---
+++ JavaVersion/src/javaversion/MyClassifier.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaversion;
+
+import weka.classifiers.Classifier;
+
+/**
+ *
+ * @author user
+ */
+public class MyClassifier {
+    Classifier classifier;
+    String name;
+}
